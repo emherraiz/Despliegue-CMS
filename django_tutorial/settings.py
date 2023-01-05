bce52e43303ece132ca0e4af89329527b42e49a7
Enrique Marina <eherraiz@myuax.com>
2023-01-05 20:44:32 +0100

    ajustamos para mysql

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
